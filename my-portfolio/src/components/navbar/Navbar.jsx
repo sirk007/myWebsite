66c4594f77ac6f7db79ff9e4f5c5d0914b4a905a
sirk007 <sirk007@gmail.com>
2023-10-30 23:42:25 +0000

    navBar components created

--- my-portfolio/src/components/navbar/Navbar.jsx
+++ my-portfolio/src/components/navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin} from 'react-icons/ri';
+import logo from '../../assets/logo.png'
 import './navbar.css';
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className="profile__navbar">
+            <div className="profile__navbar-links">
+                <div className="profile__navbar-links_logo">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="profile__navbar-links_container">
+                    <p><a href="#about">About</a></p>
+                    <p><a href="#experience">Experience</a></p>
+                    <p><a href="#projects">Projects</a></p>
+                    <p><a href="#resume">Resume</a></p>
+                </div>
+                <div className="profile__navbar-sign">
+                    <p>Download Resume</p>
+                    <button type="button">Download</button>
+                </div>
+            </div>
         </div>
     )
 }
